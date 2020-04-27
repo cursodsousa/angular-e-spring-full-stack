c9a996194a8d07a1dd4cf6203d3a4fe8fad178e4
Dougllas Sousa <cursodsousa@gmail.com>
2020-04-26 22:26:59 -0300

    concluindo intercpetor http

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +15,7 @@ import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FormsModule } from '@angular/forms';
+import { TokenInterceptor } from './token.interceptor';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,12 @@ import { FormsModule } from '@angular/forms';
   ],
   providers: [
     ClientesService,
-    ServicoPrestadoService
+    ServicoPrestadoService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
---
+++ clientes-app/src/app/token.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    const tokenString = localStorage.getItem('token')
+
+    if(tokenString){
+      const token = JSON.parse(tokenString);
+      const access_token = token.access_token;
+      console.log('access_token ', access_token)
+      request = request.clone({
+        setHeaders : {
+          Authorization: 'Bearer '+ access_token
+        }
+      })
+
+    }
+
+
+    return next.handle(request);
+  }
+}
